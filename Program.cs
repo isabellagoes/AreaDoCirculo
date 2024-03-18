91a576b1062d2f0b7d1068b1ad34f80c3f339ea7
isabellagoes <141466024+isabellagoes@users.noreply.github.com>
2024-03-18 00:56:05 +0000

    .

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double area, raio;
+double n = 3.14159;
+
+Console.WriteLine("Informe o valor de raio para o programa calcular a área de seu respectivo círculo:\n");   
+
+Console.Write("Raio do círculo: ");
+raio = Convert.ToDouble(Console.ReadLine());
+        
+area = n*Math.Pow(raio, 2);
+        
+Console.WriteLine($"Área do círculo: {area}");
